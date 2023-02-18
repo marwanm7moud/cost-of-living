a61b4c70dd70345b50bb0fd3d760d90239e6f162
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-02-18 13:51:31 +0200

    add fakedatasource for city with cheapest internet price interactor and add test cases

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,18 +6,22 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(): CityEntity{
+    fun execute(): CityEntity?{
         return dataSource
             .getAllCitiesData()
             .filter (::excludeNullSalariesAndLowQualityDataAndNullInternetConnection)
             .sortedBy (::sortByAverageBetweenNetSalaryAndInternetPrice)
-            .first()
+            .run {
+                if (isEmpty())
+                    throw Exception("There is no enough data")
+                else
+                    first()
+            }
     }
 
     private fun excludeNullSalariesAndLowQualityDataAndNullInternetConnection(city: CityEntity): Boolean {
-        return city.averageMonthlyNetSalaryAfterTax != null
-                && city.dataQuality
-                && city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!=null
+        return city.averageMonthlyNetSalaryAfterTax != null &&
+                city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!=null
     }
     private fun sortByAverageBetweenNetSalaryAndInternetPrice(city: CityEntity):Float{
         return city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!! / city.averageMonthlyNetSalaryAfterTax!!
