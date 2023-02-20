3fea965a2add8f2b85bccf526a45631d51b08d23
Hassan Ayman Ahmed <hassan3ayman@gmail.com>
2023-02-20 21:48:08 +0200

    unComment the other testFiles

--- src/main/kotlin/interactor/GetCitiesNameHasHighestSuitableClothesPriceInteractor.kt
+++ src/main/kotlin/interactor/GetCitiesNameHasHighestSuitableClothesPriceInteractor.kt
@@ -16,15 +16,15 @@ class GetCitiesNameHasHighestSuitableClothesPriceInteractor(
     }
 
     private fun excludeAnyNullClothPriceAndLowQualityData(city:CityEntity): Boolean{
-        return city.dataQuality
-                && city.clothesPrices.onePairOfJeansLevis50oneOrSimilar != null
-                && city.clothesPrices.onePairOfMenLeatherBusinessShoes != null
-                && city.clothesPrices.onePairOfNikeRunningShoesMidRange != null
-                && city.clothesPrices.oneSummerDressInAChainStoreZaraHAndM != null
+        return  city.dataQuality &&
+                city.clothesPrices.onePairOfJeansLevis50oneOrSimilar != null &&
+                city.clothesPrices.onePairOfMenLeatherBusinessShoes != null &&
+                city.clothesPrices.onePairOfNikeRunningShoesMidRange != null &&
+                city.clothesPrices.oneSummerDressInAChainStoreZaraHAndM != null
     }
 
     private fun getSumOfClothesPricesOfCity(city:CityEntity): Float{
-        return city.clothesPrices.onePairOfJeansLevis50oneOrSimilar!! +
+        return  city.clothesPrices.onePairOfJeansLevis50oneOrSimilar!! +
                 city.clothesPrices.onePairOfMenLeatherBusinessShoes!! +
                 city.clothesPrices.onePairOfNikeRunningShoesMidRange!! +
                 city.clothesPrices.oneSummerDressInAChainStoreZaraHAndM!!
